9d07ab6f6e6af6c779de81b1a35657329c4d7cab
Viktor_Baratejj <Viktor_baratejj@ukr.net>
2025-01-15 13:06:35 +0200

    add fixed nomder

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,14 @@
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
 import s from './Contact.module.css';
+
 const Contact = ({ contact }) => {
   const dispatch = useDispatch();
-
   return (
     <div className={s.item_wrapper}>
       <li className={s.contact_item}>
         <p className={s.contact_info}>
-          {contact.name}: {contact.phone}
+          {contact.name}: {contact.numder}
         </p>
         <button
           className={s.contact_btn}
